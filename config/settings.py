da5f0af01df118393af89d8c837ff073a6f408ff
Ali Khaitov <ali20032508@email.com>
2025-07-22 13:24:53 +0500

    Fix allowed host

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-local-secret-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = [
+    "web-production-f33c4.up.railway.app",
+    "127.0.0.1",
+    "localhost"
+]
 
 
 INSTALLED_APPS = [
