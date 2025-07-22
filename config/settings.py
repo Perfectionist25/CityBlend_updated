6854049b643d769b5c2735aa85fdad429151b3a9
Ali Khaitov <ali20032508@email.com>
2025-07-22 16:39:48 +0500

    Fix database settings

--- config/settings.py
+++ config/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}')
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
