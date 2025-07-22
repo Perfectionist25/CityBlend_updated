ee3b43ee4b7eb6cd27c51b022b7f1a055ea2344d
Ali Khaitov <ali20032508@email.com>
2025-07-22 18:24:42 +0500

    Fix cloudinary

--- config/settings.py
+++ config/settings.py
@@ -123,18 +123,11 @@ INSTALLED_APPS += [
     'cloudinary',
 ]
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'your_cloud_name',
-    'API_KEY': 'your_api_key',
-    'API_SECRET': 'your_api_secret',
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('dtt1kltzw'),
-    'API_KEY': os.getenv('617298774716894'),
-    'API_SECRET': os.getenv('OuB7zgc6VECbTiLwCKCjlQ2pJJ8'),
-}
+print("Cloudinary:", os.getenv('CLOUDINARY_CLOUD_NAME'))
